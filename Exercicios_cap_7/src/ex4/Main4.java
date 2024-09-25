19bac74402061dbdd7b23624efff5d9891061115
devJhonatanL <jhonatan.profissional123@gmail.com>
2024-09-24 22:35:38 -0300

    Atualização ex4 e ex5 cap 7

--- Exercicios_cap_7/src/ex4/Main4.java
+++ Exercicios_cap_7/src/ex4/Main4.java
@@ -1,30 +1,39 @@
 package ex4;
 import java.util.*;
 public class Main4 {
-
+		
 		public static boolean primo(int numero, int divisor) {
-		 
+		 // valida se o numero é menor que dois
 		if (numero <2) {
 			return false;
-			
+		//valida se o numero é dois, caso seja ele ja retorna que é verdade
 		} if (numero == divisor) {
 			return true;
-			
+		// valida se o numero divide pelo divisor 	
 		} if (numero % divisor ==0) {
 			return false;
-			
 		} 
+		// caso  nenhuma condição seja atendida ele chama a função aumentando o valor do divisor ate que uma das condições seja atendida
 			return primo(numero, divisor + 1);
 			
 		}
-			
+		// metodo main	
+		
 		public static void main(String[] args) {	
+		//declara que o divisor inicia com o valor 2
 		
-		int divisor =2;	
+			int divisor =2;	
+		// cria o scanner para coletar o numero a ser verificado
+		
 		Scanner coletor = new Scanner(System.in);
-		System.out.println("Informe um numero");
+		// printa a pergunta 
 		
-		int numero = coletor.nextInt();
+		System.out.println("Informe um numero");
+		// salva o valor do numero
+		
+		int numero = coletor.nextInt();  
+		
+		// chama a função
 		
 		if(primo(numero, divisor)) {
 			System.out.println("O numero "+numero+" é primo");
--- Exercicios_cap_7/src/ex5/Main5.java
+++ Exercicios_cap_7/src/ex5/Main5.java
@@ -1,10 +1,30 @@
 package ex5;
-
+import java.util.*;
 public class Main5 {
 
+public static String inverteString(String texto) {
+		
+		// primeira condição onde avalia se o parâmetro  textp esta vazia. Caso esteja vai retornar o texto.
+		if(texto.isEmpty()) {
+			return texto;
+		}else {
+			/*inicia a recursividade, pegando o primeiro letra da palavra, removendo ela da palavra e mandando para a ultima posição invertendo
+			 a ordem da palavra*/
+			return inverteString(texto.substring(1)) + 
+					texto.charAt(0);
+		}
+		
+	}
+	
+	// metodo main
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//coletor da palavra a ser invertida
+		Scanner coletor = new Scanner(System.in);
+		
+		System.out.println("Informe uma palavra para ser invertida");
+		String texto = coletor.next();
+		//printa na tela
+		System.out.println(inverteString(texto));
 	}
 
 }
