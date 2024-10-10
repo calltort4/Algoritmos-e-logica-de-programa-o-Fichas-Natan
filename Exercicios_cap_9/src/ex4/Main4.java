1ab760810187156328b6a0a7777749f8a15c5d08
devJhonatanL <jhonatan.profissional123@gmail.com>
2024-10-09 23:19:51 -0300

    lista 9

--- Exercicios_cap_9/src/ex4/Main4.java
+++ Exercicios_cap_9/src/ex4/Main4.java
@@ -1,17 +1,28 @@
 package ex4;
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class Main4 {
 
 		
 		public static void main(String[] args) throws IOException  {	
 		//declara que o divisor inicia com o valor 2
 		
-		BufferedReader leitor = new BufferedReader(new FileReader("arquivo.csv"));
-		String linha;
-			
-		while ((linha = leitor.readLine()) != null) {
-			System.out.println(linha);
+		try (BufferedReader leitor = new BufferedReader(new FileReader("arquivo1.csv"))) {
+			String linha;
+				
+			while ((linha = leitor.readLine()) != null) {
+				String[] dados = linha.split(";");
+				String nome = dados[0];
+				double nota1 = Double.parseDouble(dados[1]);
+				double nota2 = Double.parseDouble(dados[2]);
+				double nota3 = Double.parseDouble(dados[3]);
+
+				double media = (nota1 + nota2 + nota3) / 3;
+				System.out.printf("%s - Média: %.2f%n ", nome, media);
+		    }
+        } catch (IOException e) {
+            e.printStackTrace();
 		}
 			
 			
